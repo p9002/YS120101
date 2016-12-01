3f88d9944bceb0b3b6aea3fa4dca9096ba1cdf78
alan <p9002>
2016-12-01 16:29:53 +0800

    即時讀取pi2的溫度,及推播

---
+++ app/src/main/java/alan/no1/ys120101/MyService.java
@@ -0,0 +1,23 @@
+package alan.no1.ys120101;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyService extends FirebaseInstanceIdService {
+    public MyService() {
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FCM", "Refreshed token: " + refreshedToken);
+        // TODO: Implement this method to send any registration to your app's servers.
+        // sendRegistrationToServer(refreshedToken);
+    }
+}
